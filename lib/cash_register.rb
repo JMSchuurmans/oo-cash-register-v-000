a747e71a1ddee3f2289b20d633349360398efcdc
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-06-16 15:50:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
 
+  attr_accessor :cash_register, :cash_register_with_discount, :discount, :total
+
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(item, price, quantity = nil)
+    @total = price
+
+  end
+
+
+
+
+
+
+
+
+
+end
